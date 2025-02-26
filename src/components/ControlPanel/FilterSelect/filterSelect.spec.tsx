b73ea47591fb2f9ab34c46946a1dbecc1c9b3ba9
Bruno Tenório <tenoriobn@gmail.com>
2025-02-26 17:16:18 -0300

    26-02-25/fix - ajustando teste do (filterSelect.spec.tsx)

--- src/components/ControlPanel/FilterSelect/filterSelect.spec.tsx
+++ src/components/ControlPanel/FilterSelect/filterSelect.spec.tsx
@@ -4,7 +4,7 @@ import { RecoilObserver } from "src/tests/utils/recoilObserver";
 import { vi } from "vitest";
 import FilterSelect from ".";
 import { countryFilterState } from "src/common/states/atom";
-import { fireEvent, screen } from "@testing-library/react";
+import { fireEvent, screen, within } from "@testing-library/react";
 import options from "./options.json";
 
 const renderWithRecoil = () => {
@@ -22,70 +22,73 @@ const renderWithRecoil = () => {
 
 describe('<FilterSelect />', () => { 
   let listOptions: HTMLElement;
+  let selectedOption: HTMLElement;
 
   beforeEach(() => {
     renderWithRecoil();
 
-    listOptions = screen.getByTestId('list-options');
+    listOptions = screen.getByRole('list');
+    selectedOption =  screen.getByRole('option', { selected: true });
   });
 
-  const openListAndCheckVisibility = () => {
-    const filterSelect = screen.getByTestId('filter-select');
-    fireEvent.click(filterSelect);
+  const openList = () => {
+    fireEvent.click(screen.getByRole('combobox'));
     expect(listOptions).toHaveStyle({ height: "max-content" });
   };
-
-  const selectOptionAndCheckListClosed = (optionName: string) => {
-    const option = screen.getByText(optionName);
-    fireEvent.click(option);
-    
-    const selectedOption = screen.getByTestId('selected-option');
-    
+  
+  const closeList = () => {
+    expect(listOptions).toHaveStyle({ height: "0" });
+  };
+  
+  const selectOption = (optionName: string) => {
+    const optionElement = within(listOptions).getByText(optionName);
+    fireEvent.click(optionElement);
+  };
+  
+  const verifySelectedOption = (optionName: string) => {
     if (optionName === "Remove Filter") {
       expect(selectedOption).toHaveTextContent('');
     } else {
       expect(selectedOption).toHaveTextContent(optionName);
     }
-    
-    expect(listOptions).toHaveStyle({ height: "0" });
   };
-
-  it('should render the component and main elements', () => { 
-    const filterSelectContainer = screen.getByTestId('filter-select-container');
-    expect(filterSelectContainer).toBeInTheDocument();
   
-    const label = screen.getByTestId('label');
-    expect(label).toBeInTheDocument();
-    expect(label).toHaveTextContent('Filter by Region');
+  it('Should render the FilterSelect component and required elements', () => { 
+    expect(screen.getByLabelText('Filter by Region')).toBeInTheDocument();
+    expect(screen.getByTitle('select-arrow')).toBeInTheDocument();
   });
   
-  it('should render list options without clear selected option', () => {
+  it('Should render the hidden list with the default options, excluding the "Remove Filter" option.', () => {
     expect(listOptions).toBeInTheDocument();
-    expect(listOptions).toHaveStyle({ height: "0" });
+    closeList();
   
     const listItems = screen.getAllByRole('listitem');
-    const initialListItems = options.filter(option => option.id !== 0);
-    expect(listItems).toHaveLength(initialListItems.length);
+    const filteredOptions = options.filter(option => option.id !== 0);
   
-    initialListItems.forEach((option, index) => {
-      expect(listItems[index]).toHaveTextContent(option.name);
+    listItems.forEach((option, index) => {
+      expect(option).toHaveTextContent(filteredOptions[index].name);
     });
+  
+    expect(listItems).toHaveLength(filteredOptions.length);
   });
-
-  it('should open a list when clicked, render the selected option, and close the list when clicking an option', () => {
-    openListAndCheckVisibility();
-
-    const optionToClick = options[1].name;
-    selectOptionAndCheckListClosed(optionToClick);
+  
+  it('Should toggle the list visibility on click, display the selected option, and close the list upon selecting an option.', () => {
+    openList();
+    selectOption('Africa');
+    verifySelectedOption('Africa');
+    closeList();
   });
-
-  it('should open a list with the option to clear the selected option', () => {
-    openListAndCheckVisibility();
-    const optionToClick = options[1].name;
-    selectOptionAndCheckListClosed(optionToClick);
-
-    openListAndCheckVisibility();
-    const removeFilterOption = options[0].name;
-    selectOptionAndCheckListClosed(removeFilterOption);
+  
+  it('Should open a list with the option to clear the selected option', () => {
+    openList();
+    selectOption('Africa');
+    verifySelectedOption('Africa');
+    closeList();
+  
+    openList();
+    selectOption('Remove Filter');
+    verifySelectedOption('Remove Filter');
+    closeList();
   });
+  
 });
